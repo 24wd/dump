1b0371986a571a716db87d8c92950c040f818b05
24wd <118280413+24wd@users.noreply.github.com>
2022-12-13 05:44:39 -0800

    Create testout.js

---
+++ testout.js
@@ -0,0 +1,32 @@
+clear();
+console.log('Start Script');
+//Grab Score Text
+const container = document.getElementById('simScore');
+//Split Score Text Characters into Numbers
+let arr = container.textContent.split(' ');
+//Split Numbers by /
+let pgrade = arr[1].split('/');
+// Grab the Max Score
+let pgradeneed = pgrade[1];
+//Set new Score
+container.textContent = 'Score: ' + pgradeneed + '/' + pgradeneed + ' (100%)';
+//Set Progress bar width to 100%
+document.getElementsByClassName('pbProgressLine')[0].style.width = "100%";
+//Generate new Time with 2 decimal places between 20-5
+var gennewtime = (Math.random() * (20.00 + 5.00) + 5.00).toFixed(2);
+//Replace decimal with : and set text
+document.getElementsByClassName("timeSpent")[0].textContent = 'Time Spent: ' + gennewtime.toString().replace('.', ':');
+//Grab all X marks with their class names
+let a = document.getElementsByClassName( "icon-close" );
+// Add  check mark class
+[...a].forEach( x => x.className += " icon-check" );
+// Remove X mark class
+[...a].forEach( x => x.classList.remove("icon-close") );
+//Grab all incorrect style classes
+let b = document.getElementsByClassName( "clsTaskCreditIncorrect3" );
+// Add correct style to incorrect
+[...b].forEach( z => z.className += " clsTaskCreditCorrect3" );
+// Remove Incorrect style class
+[...b].forEach( z => z.classList.remove("clsTaskCreditIncorrect3") );
+//Log to console done
+console.log("Finished");
